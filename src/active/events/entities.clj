df83c7f468d39c6b1a36d6d0956ba61004e00966
David Frese <david.frese@active-group.de>
2022-07-20 18:19:04 +0200

    Add utils for 'crud' events on entites.

---
+++ src/active/events/entities.clj
@@ -0,0 +1,95 @@
+(ns active.events.entities
+  "Functions to define events that represent create/update/delete actions on entities."
+  (:require [active.events.core :as core]))
+
+(defmacro ^:private define-tagged-type [tag ctor predicate & [values]]
+  `(do
+     (defn ~ctor [~@values]
+       [~tag ~@values])
+     (defn ~predicate [v#]
+       (and (vector? v#) (= ~tag (first v#))))
+
+     ~@(map-indexed (fn [idx v]
+                      `(defn ~v [v#]
+                         (assert (~predicate v#) v#)
+                         (get v# ~(inc idx))))
+                    values)))
+
+(define-tagged-type ::create-entity
+  create-entity create-entity?
+  [create-entity-value])
+
+(define-tagged-type ::delete-entity
+  delete-entity delete-entity?)
+
+(define-tagged-type ::update-entity
+  update-entity update-entity?
+  [update-entity-patch])
+
+(defn- to-entity* [apply-patch & [default]]
+  (fn
+    ([] default)
+    ([res v]
+     (cond
+       (create-entity? v) (create-entity-value v)
+       (update-entity? v) (apply-patch res (update-entity-patch v))
+       (delete-entity? v) default
+       :else res))))
+
+(defn- on-value [f]
+  (fn
+    ([] (f))
+    ([res event]
+     (f res (core/event-value event)))))
+
+(defn to-entity [apply-patch & [default]]
+  (on-value (to-entity* apply-patch default)))
+
+;; TODO add/allow meta data for users that want to record 'who did it'? :-/
+
+(defn- to-entity-history* [created updated deleted]
+  (fn
+    ([] [])
+    ([res time v]
+     (cond
+       (create-entity? v) (conj res (created time (create-entity-value v))) 
+       (update-entity? v) (conj res (updated time (update-entity-patch v)))
+       (delete-entity? v) (conj res (deleted time))
+       :else res))))
+
+(defn- on-time-and-value [f]
+  (fn
+    ([] (f))
+    ([res event]
+     (f res (core/event-time event) (core/event-value event)))))
+
+(defn to-entity-history [created updated deleted]
+  (on-time-and-value (to-entity-history* created updated deleted)))
+
+(define-tagged-type ::entity-reference
+  entity-reference entity-reference?
+  [entity-reference-id entity-reference-action])
+
+(defn- to-reference-map [f]
+  (let [empty (f)]
+    (fn
+      ([] {})
+      ([res event]
+       (let [time (core/event-time event)
+             v (core/event-value event)]
+         (cond
+           (entity-reference? v)
+           (let [id (entity-reference-id v)]
+             (assoc res id
+                    (f (get res id empty) (core/event time (entity-reference-action v)))))
+
+           :else res))))))
+
+(defn to-entities-map [apply-patch & [default]]
+  (to-reference-map (to-entity apply-patch default)))
+
+(defn to-entities-history [created updated deleted]
+  (to-reference-map (to-entity-history created updated deleted)))
+
+;; ...same on domains (multiple kinds) of entities?
+
---
+++ test/active/events/entities_test.clj
@@ -0,0 +1,34 @@
+(ns active.events.entities-test
+  (:require [clojure.test :refer (deftest testing is)]
+            [active.events.core :as core]
+            [active.events.entities :as e])
+  (:import [java.time Instant]))
+
+(deftest entities-map-test
+  (let [src (core/new-memory-event-source)
+
+        t1 (Instant/ofEpochSecond 1000001)
+        t2 (Instant/ofEpochSecond 1000002)
+        t3 (Instant/ofEpochSecond 1000003)
+        
+        patch-entity (fn [e p]
+                       (update e :a concat (:a p)))]
+    
+    (core/add-events! src [(core/event t1 (e/entity-reference :foo (e/create-entity {:a [:bar]})))
+                           (core/event t2 (e/entity-reference :foo (e/update-entity {:a [:baz]})))])
+
+    (is (= {:foo {:a [:bar :baz]}}
+           (core/reduce-events src
+                               (e/to-entities-map patch-entity))))
+
+    (core/add-events! src [(core/event t3 (e/entity-reference :foo (e/delete-entity)))])
+    (is (= {:foo [[:created t1 {:a [:bar]}]
+                  [:updated t2 {:a [:baz]}]
+                  [:deleted t3]]}
+           (core/reduce-events src
+                               (e/to-entities-history (fn [t v] [:created t v])
+                                                      (fn [t v] [:updated t v])
+                                                      (fn [t] [:deleted t])))))
+    
+    )
+  )
